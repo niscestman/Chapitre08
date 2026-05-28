c22b93bfea45d9d6b6a8e7458316ef44c0b67a4c
nicestman <ouattaraali941@gmail.com>
2026-05-28 12:36:29 +0200

    Amelioration des test avec l'utilisation de BeforeEach

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -2,11 +2,19 @@ package bookstoread;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BookShelfSpec {
+
+    private BookShelf shelf;
+
+    @BeforeEach
+    void init() throws Exception {
+        shelf = new BookShelf();
+    }
     @Test
     public void shelfEmptyWhenNoBookAdded() throws Exception {
         BookShelf shelf = new BookShelf();
